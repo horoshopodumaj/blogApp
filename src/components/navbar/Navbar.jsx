11a65248ee70dccb9fd4a710e299cb337c6ef37d
Diana Murtazina <murka8831@yandex.ru>
2023-09-18 23:26:32 +0300

    added navbar menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import AuthLinks from "../authLinks/AuthLinks";
+import ThemeToggle from "../themeToggle/ThemeToggle";
 import styles from "./navbar.module.css";
 
 export const Navbar = () => {
-    return <div className={styles.container}>Navbar</div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.social}>
+                <Image src="/telega.png" alt="telega" width={24} height={24} />
+                <Image src="/vk.png" alt="vk" width={24} height={24} />
+                <Image src="/youtube.png" alt="youtube" width={24} height={24} />
+            </div>
+            <div className={styles.logo}>Blog</div>
+            <div className={styles.links}>
+                <ThemeToggle />
+                <Link href="/">Homepage</Link>
+                <Link href="/">Contact</Link>
+                <Link href="/">About</Link>
+                <AuthLinks />
+            </div>
+        </div>
+    );
 };
