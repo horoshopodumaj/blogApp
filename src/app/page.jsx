6b2ef93104822d93bc24dc20052d1210eac0db32
Diana Murtazina <murka8831@yandex.ru>
2023-09-18 17:46:13 +0300

    added new components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,18 @@
-import Link from "next/link";
+import CardList from "@/components/cardList/CardList";
+import CategoryList from "@/components/categoryList/CategoryList";
+import Featured from "@/components/featured/Featured";
+import Menu from "@/components/menu/Menu";
+import styles from "./homepage.module.css";
 
 export default function Home() {
     return (
-        <div>
-            <Link href="/">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse earum tenetur quidem maiores soluta. Molestias quas, eius nam impedit sint magni, non ut laborum, eos saepe vitae tempora
-                eveniet reprehenderit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse earum tenetur quidem maiores soluta. Molestias quas, eius nam impedit sint magni, non ut laborum,
-                eos saepe vitae tempora eveniet reprehenderit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse earum tenetur quidem maiores soluta. Molestias quas, eius nam impedit sint
-                magni, non ut laborum, eos saepe vitae tempora eveniet reprehenderit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse earum tenetur quidem maiores soluta. Molestias
-                quas, eius nam impedit sint magni, non ut laborum, eos saepe vitae tempora eveniet reprehenderit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse earum tenetur quidem
-                maiores soluta. Molestias quas, eius nam impedit sint magni, non ut laborum, eos saepe vitae tempora eveniet reprehenderit? Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Esse earum tenetur quidem maiores soluta. Molestias quas, eius nam impedit sint magni, non ut laborum, eos saepe vitae tempora eveniet reprehenderit? Lorem ipsum dolor sit amet
-                consectetur, adipisicing elit. Esse earum tenetur quidem maiores soluta. Molestias quas, eius nam impedit sint magni, non ut laborum, eos saepe vitae tempora eveniet reprehenderit?
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse earum tenetur quidem maiores soluta. Molestias quas, eius nam impedit sint magni, non ut laborum, eos saepe vitae tempora
-                eveniet reprehenderit?
-            </Link>
+        <div className={styles.container}>
+            <Featured />
+            <CategoryList />
+            <div className={styles.content}>
+                <CardList />
+                <Menu />
+            </div>
         </div>
     );
 }
