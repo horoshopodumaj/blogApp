8681a044df5a4e1258498beaf0e437d79cb6e6a6
Diana Murtazina <murka8831@yandex.ru>
2023-09-18 16:51:54 +0300

    added responsive styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,20 @@
-import styles from "./homepage.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello</div>;
+    return (
+        <div>
+            <Link href="/">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse earum tenetur quidem maiores soluta. Molestias quas, eius nam impedit sint magni, non ut laborum, eos saepe vitae tempora
+                eveniet reprehenderit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse earum tenetur quidem maiores soluta. Molestias quas, eius nam impedit sint magni, non ut laborum,
+                eos saepe vitae tempora eveniet reprehenderit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse earum tenetur quidem maiores soluta. Molestias quas, eius nam impedit sint
+                magni, non ut laborum, eos saepe vitae tempora eveniet reprehenderit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse earum tenetur quidem maiores soluta. Molestias
+                quas, eius nam impedit sint magni, non ut laborum, eos saepe vitae tempora eveniet reprehenderit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse earum tenetur quidem
+                maiores soluta. Molestias quas, eius nam impedit sint magni, non ut laborum, eos saepe vitae tempora eveniet reprehenderit? Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Esse earum tenetur quidem maiores soluta. Molestias quas, eius nam impedit sint magni, non ut laborum, eos saepe vitae tempora eveniet reprehenderit? Lorem ipsum dolor sit amet
+                consectetur, adipisicing elit. Esse earum tenetur quidem maiores soluta. Molestias quas, eius nam impedit sint magni, non ut laborum, eos saepe vitae tempora eveniet reprehenderit?
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse earum tenetur quidem maiores soluta. Molestias quas, eius nam impedit sint magni, non ut laborum, eos saepe vitae tempora
+                eveniet reprehenderit?
+            </Link>
+        </div>
+    );
 }
