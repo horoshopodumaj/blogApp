d5be3ee7e9d9833eeff56ecefab09dfe08f471ba
Diana Murtazina <murka8831@yandex.ru>
2023-09-19 13:57:57 +0300

    category list design

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,7 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
 import styles from "./categoryList.module.css";
 
 const CategoryList = () => {
-    return <div className={styles.container}>CategoryList</div>;
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Popular Category</h1>
+            <div className={styles.categories}>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/style.png" alt="" width={32} height={32} className={styles.image} />
+                    Style
+                </Link>
+                <Link href="/blog" className={`${styles.category} ${styles.fashion}`}>
+                    <Image src="/fashion.png" alt="" width={32} height={32} className={styles.image} />
+                    Fasion
+                </Link>
+                <Link href="/blog" className={`${styles.category} ${styles.food}`}>
+                    <Image src="/food.png" alt="" width={32} height={32} className={styles.image} />
+                    Food
+                </Link>
+                <Link href="/blog" className={`${styles.category} ${styles.travel}`}>
+                    <Image src="/travel.png" alt="" width={32} height={32} className={styles.image} />
+                    Travel
+                </Link>
+                <Link href="/blog" className={`${styles.category} ${styles.culture}`}>
+                    <Image src="/culture.png" alt="" width={32} height={32} className={styles.image} />
+                    Culture
+                </Link>
+                <Link href="/blog" className={`${styles.category} ${styles.coding}`}>
+                    <Image src="/coding.png" alt="" width={32} height={32} className={styles.image} />
+                    Coding
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default CategoryList;
