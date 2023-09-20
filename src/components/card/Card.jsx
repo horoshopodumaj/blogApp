a975f21574543ea02c63f820484be0a3eb5d7b44
Diana Murtazina <murka8831@yandex.ru>
2023-09-20 17:34:45 +0300

    single post

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,19 +5,21 @@ import styles from "./card.module.css";
 const Card = ({ item }) => {
     return (
         <div className={styles.container}>
-            <div className={styles.imageContainer}>
-                <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-            </div>
+            {item.img && (
+                <div className={styles.imageContainer}>
+                    <Image src={item.img} alt="" fill className={styles.image} />{" "}
+                </div>
+            )}
             <div className={styles.textContainer}>
                 <div className={styles.detail}>
                     <span className={styles.date}>{item.createdAt.substring(0, 10)} - </span>
                     <span className={styles.category}>{item.catSlug}</span>
                 </div>
-                <Link href="/">
+                <Link href={`/posts/${item.slug}`}>
                     <h1>{item.title}</h1>
                 </Link>
-                <p className={styles.desc}>{item.desc}</p>
-                <Link href="/" className={styles.link}>
+                <p className={styles.desc}>{item.desc.substring(0, 60)}</p>
+                <Link href={`/posts/${item.slug}`} className={styles.link}>
                     Read More
                 </Link>
             </div>
